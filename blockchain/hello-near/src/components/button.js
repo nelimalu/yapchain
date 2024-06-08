d7e4be638520fb39bb09959bd471ea67c7834d29
Louis Berenyi <sanicisspeedandfast@gmail.com>
2024-06-08 19:04:28 -0400

    Added redirection using next router for users who are signed in

--- blockchain/hello-near/src/components/button.js
+++ blockchain/hello-near/src/components/button.js
@@ -1,21 +1,23 @@
 //button.js
 import Link from 'next/link';
 import { useEffect, useState, useContext } from 'react';
-import { redirect } from 'next/navigation';
+
 import { NearContext } from '@/context';
+import { useRouter } from 'next/router';
 
 export default function Button() {
     const { signedAccountId, wallet } = useContext(NearContext);
     const [action, setAction] = useState(() => { });
     const [label, setLabel] = useState('Loading...');
-
+    const router = useRouter()
     useEffect(() => {
       if (!wallet) return;
-
+        
       if (signedAccountId) {
+        router.push("/yapper")
+        
         setAction(() => wallet.signOut);
         setLabel(`Logout ${signedAccountId}`);
-        redirect('/yapper')
       } else {
         setAction(() => wallet.signIn);
         setLabel('Login');

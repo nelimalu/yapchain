05bab35a182ce6838999798d356eef6f325fa616
nelimalu <lukaj2501@gmail.com>
2024-06-08 16:58:35 -0400

    moving button

---
+++ blockchain/hello-near/src/components/button.js
@@ -0,0 +1,28 @@
+//button.js
+import Link from 'next/link';
+import { useEffect, useState, useContext } from 'react';
+
+import { NearContext } from '@/context';
+
+export default function Button() {
+    const { signedAccountId, wallet } = useContext(NearContext);
+    const [action, setAction] = useState(() => { });
+    const [label, setLabel] = useState('Loading...');
+
+    useEffect(() => {
+      if (!wallet) return;
+
+      if (signedAccountId) {
+        setAction(() => wallet.signOut);
+        setLabel(Logout ${signedAccountId});
+      } else {
+        setAction(() => wallet.signIn);
+        setLabel('Login');
+      }
+    }, [signedAccountId, wallet]);
+    return (
+        <div className='navbar-nav pt-1'>
+          <button className="btn btn-secondary" onClick={action} > {label} </button>
+        </div>
+    )
+}

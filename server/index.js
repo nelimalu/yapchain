107996d44c69d63910e263b2232fc8c3ead508eb
Louis Berenyi <sanicisspeedandfast@gmail.com>
2024-06-08 11:13:35 -0400

    dangerous html

--- server/index.js
+++ server/index.js
@@ -1,7 +1,14 @@
 const express = require('express')
 const app = express()
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
 
 app.get("/message", (req,res) => {
+    console.log("received")
     res.send("I GOT YOUR MESSAGE!!!")
 })
 

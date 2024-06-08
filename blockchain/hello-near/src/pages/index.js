7a873499bbef32ec2f6b86630755a38365bec6c5
nelimalu <lukaj2501@gmail.com>
2024-06-08 13:54:42 -0400

    migrated login even more

--- blockchain/hello-near/src/pages/index.js
+++ blockchain/hello-near/src/pages/index.js
@@ -2,37 +2,22 @@ import Image from 'next/image';
 
 import NearLogo from '/public/near.svg';
 import NextLogo from '/public/next.svg';
-import styles from '@/styles/app.module.css';
+/*import '../styles/login.css';*/
 import { Cards } from '@/components/cards';
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}> </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src={NearLogo}
-          alt="NEAR Logo"
-          width={110 * 1.5}
-          height={28 * 1.5}
-          priority
-        />
-        <h3 className="ms-2 me-3 text-dark"> + </h3>
-        <Image
-          className={styles.logo}
-          src={NextLogo}
-          alt="Next.js Logo"
-          width={300 * .58}
-          height={61 * .58}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <Cards />
-      </div>
-    </main>
+    <>
+      <div style={{height: "100vh"}} className="w-full flex justify-center items-center">
+        <div className='login min-w-96 min-h-96 px-8 pt-8 flex flex-col gap-4'>
+          <div className="flex items-center justify-center w-full mb-2"><h1 className='text-5xl font-bold text-white'>YapChain</h1></div>
+            <input name='username' className="px-2 py-1 pixel-corners"placeholder='username'/>
+            <input name='password' className="px-2 py-1 pixel-corners"placeholder='password'/>
+            
+            <div className='submit flex justify-center items-center'><p className='font-bold'>Go!</p></div>
+          </div>
+        </div>
+      <div className={`w-full h-full z-10 bg-white`} ></div>
+    </>
   );
 }

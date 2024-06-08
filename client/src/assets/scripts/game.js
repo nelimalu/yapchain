44b42e1ff8b00c5f4ffd7cd973f7f9e985a360ff
nelimalu <lukaj2501@gmail.com>
2024-06-08 11:58:03 -0400

    added walk cycle

--- client/src/assets/scripts/game.js
+++ client/src/assets/scripts/game.js
@@ -29,16 +29,127 @@ function Sprite(image, x, y, width, height, src_x, src_y, src_width, src_height)
   }
 }
 
-var sprites = [
-  new Sprite("./../images/character/ycSpriteMForward.png", 0, 0, 16 * 4, 22 * 4, 0, 0, 16, 22)
-];
+var pscale = 5;
+var velocity = 3;
+var background = new Sprite("./../images/background.png", -100, -100, 1906, 1058, 0, 0, 1906, 1058);
+
+class Player {
+
+  constructor() {
+    this.x = window.innerWidth / 2 - 8 * pscale;
+    this.y = window.innerHeight / 2 - 11 * pscale;
+    this.width = 16 * pscale;
+    this.height = 22 * pscale;
+    this.pressed = [false, false, false, false];  // left right up down
+    this.playersprites = [
+      new Sprite("./../images/character/ycSpriteMForward.png", this.x, this.y, this.width, this.height, 0, 0, 16, 22),
+      new Sprite("./../images/character/ycSpriteMForward1.png", this.x, this.y, this.width, this.height, 0, 0, 16, 22),
+      new Sprite("./../images/character/ycSpriteMForward2.png", this.x, this.y, this.width, this.height, 0, 0, 16, 22),
+      new Sprite("./../images/character/ycSpriteMBackward.png", this.x, this.y, this.width, this.height, 0, 0, 16, 22),
+      new Sprite("./../images/character/ycSpriteMBackward1.png", this.x, this.y, this.width, this.height, 0, 0, 16, 22),
+      new Sprite("./../images/character/ycSpriteMBackward2.png", this.x, this.y, this.width, this.height, 0, 0, 16, 22),
+      new Sprite("./../images/character/ycSpriteMLeft.png", this.x, this.y, this.width, this.height, 0, 0, 16, 22),
+      new Sprite("./../images/character/ycSpriteMLeft1.png", this.x, this.y, this.width, this.height, 0, 0, 16, 22),
+      new Sprite("./../images/character/ycSpriteMLeft2.png", this.x, this.y, this.width, this.height, 0, 0, 16, 22),
+      new Sprite("./../images/character/ycSpriteMRight.png", this.x, this.y, this.width, this.height, 0, 0, 16, 22),
+      new Sprite("./../images/character/ycSpriteMRight1.png", this.x, this.y, this.width, this.height, 0, 0, 16, 22),
+      new Sprite("./../images/character/ycSpriteMRight2.png", this.x, this.y, this.width, this.height, 0, 0, 16, 22)
+    ];
+    this.cycle = 0;
+  }
+
+  draw() {
+    if (this.pressed[0]) { // left
+      this.playersprites[6 + this.cycle].draw();
+    }
+    else if (this.pressed[1]) { // right
+      this.playersprites[9 + this.cycle].draw();
+    }
+    else if (this.pressed[2]) { // up
+      this.playersprites[3 + this.cycle].draw();
+    }
+    else if (this.pressed[3]) { // down
+      this.playersprites[0 + this.cycle].draw();
+    }
+    else {
+      this.playersprites[0].draw();
+    }
 
 
+
+    if (this.pressed[0]) { // left
+      background.x += velocity;
+    }
+    if (this.pressed[1]) { // right
+      background.x -= velocity;
+
+    }
+    if (this.pressed[2]) { // up
+      background.y += velocity;
+
+    }
+    if (this.pressed[3]) { // down
+      background.y -= velocity;
+    }
+  }
+
+}
+
+
+window.addEventListener("keydown", function(event) {
+  if (event.key == "w" || event.key == "ArrowUp") {  // W key
+    player.pressed[2] = true;
+  }
+
+  if (event.key == "s" || event.key == "ArrowDown") {  // S key
+    player.pressed[3] = true;
+  }
+
+  if (event.key == "a" || event.key == "ArrowLeft") {  // A key
+    player.pressed[0] = true;
+  }
+
+  if (event.key == "d" || event.key == "ArrowRight") {  // D key
+    player.pressed[1] = true;
+  }
+
+});
+
+window.addEventListener("keyup", function(event) {
+    if (event.key == "w" || event.key == "ArrowUp") {
+      player.pressed[2] = false;
+    }
+
+    if (event.key == "s" || event.key == "ArrowDown") {
+      player.pressed[3] = false;
+    }
+
+    if (event.key == "a" || event.key == "ArrowLeft") {
+      player.pressed[0] = false;
+    }
+
+    if (event.key == "d" || event.key == "ArrowRight") {
+      player.pressed[1] = false;
+    }
+});
+
+
+
+var player = new Player();
+var frame = 0;
 function animate() {
   requestAnimationFrame(animate);
   c.clearRect(0, 0, innerWidth, innerHeight);
+  background.draw();
 
-  sprites[0].draw();
+  frame++;
+  if (frame % 10 == 0) {
+    player.cycle++;
+    player.cycle %= 3;
+  }
+  console.log(player.cycle)
+  player.draw();
+
 }
 
 animate();
